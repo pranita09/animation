e52465cc355a29086134cc0c70e652b6ef8c5ab8
Pranita <pfulsundar8@gmail.com>
2023-08-28 10:12:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import LandingImg from "./asset/Landing-Bg.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">{/* <img src={LandingImg} alt="bg" /> */}</div>
+    </div>
+  );
+}
